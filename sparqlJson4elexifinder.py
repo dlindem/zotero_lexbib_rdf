37b9a366c0df4cf07d96248151dbeaf34f8911be
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-05-10 18:28:37 +0200

    Update sparqlJson4elexifinder.py
    Implement txt object (Zotero pdf2text output) processing; if not found, take abstract text (at the moment, only English)

--- sparqlJson4elexifinder.py
+++ sparqlJson4elexifinder.py
@@ -2,7 +2,7 @@ import json
 import spacy
 sp = spacy.load('en_core_web_sm')
 
-with open("E:/results.json", encoding="utf-8") as f:
+with open("D:/Lab_LexDo/100520/result.json", encoding="utf-8") as f:
 	data =  json.load(f, encoding="utf-16")
 results = data['results']
 bindings = results['bindings']
@@ -20,9 +20,9 @@ for item in bindings:
 	if 'title' in item:
 		title = item['title']['value']
 		item['title'] = title
-	if 'pubTM' in item:
-		pubTM = item['pubTM']['value']
-		item['pubTM'] = pubTM
+	if 'articleTM' in item:
+		articleTM = item['articleTM']['value']
+		item['articleTM'] = articleTM
 	# adding '?usenewlibrary=0' to Zotero Links
 	if 'sourceUri' in item:
 		sourceUri = item['sourceUri']['value']+'?usenewlibrary=0'
@@ -30,19 +30,36 @@ for item in bindings:
 	if 'sourceTitle' in item:
 		sourceTitle = item['sourceTitle']['value']
 		item['sourceTitle'] = sourceTitle
-	if 'lang' in item:
-		lang = item['lang']['value']
+	if 'publang' in item:
+		lang = item['publang']['value']
 		item['lang'] = lang[-3:]
-	if 'body' in item and lang[-3:] == 'eng':
-		bodytext = item['body']['value']
+	bodytext = ""
+	# check if text is English and txt-object exists
+	if lang [-3:] == 'eng' and 'pdftxt' in item:
+		txtpath = item['pdftxt']['value']
+		try:
+			with open(txtpath, 'r', encoding="utf-8") as txtfile:
+				bodytext = txtfile.read()
+			item['body'] = bodytext
+		except IOError:
+			print(title+" "+txtpath+" not accessible")
+	else:
+		print(title+" is "+lang+" ...aborted")
+	# if fulltext was not English or not found, use english abstract (if exists)
+	if bodytext == "" and 'abstracttext' in item and item['abstractlang']['value'][-3:] == 'eng':
+		bodytext = item['abstracttext']['value']
 		item['body'] = bodytext
-		bodylem = ""
-		for word in sp(bodytext):
-			bodylem+=("%s " % word.lemma_)
-		item['bodylem'] = bodylem
+		print(" - got abstract text instead")
+	elif bodytext == "":
+		print(" - found no English abstract")
+	# lemmatize english text or abstract
+	bodylem = ""
+	for word in sp(bodytext):
+		bodylem+=("%s " % word.lemma_)
+	item['bodylem'] = bodylem
 		
 
 
-with open('E:\processed.json', 'w') as json_file:
+with open('D:/Lab_LexDo/100520/processed.json', 'w') as json_file:
 	json.dump(bindings, json_file, indent=2)
 
