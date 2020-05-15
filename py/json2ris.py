f038abff0cf3492e75c255418ac7d64f4f571066
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-05-16 00:57:13 +0200

    Create json2ris.py

---
+++ py/json2ris.py
@@ -0,0 +1,14 @@
+#converts json to ris (json key to ris key, json value to ris value), by dlindem
+import json
+
+with open("D:/EuralexMerge/mergedris.json", encoding="utf-8") as f:
+	jsonfile =  json.load(f, encoding="utf-8")
+
+print(jsonfile)
+
+with open('D:/EuralexMerge/mergedris.ris', 'w', encoding="utf-8") as risfile:
+	for item in jsonfile:
+		for key, valuelist in item.items():
+			for value in valuelist:
+				risfile.write(key+"  - "+value+"\n")
+		risfile.write("ER  - \n\n")
