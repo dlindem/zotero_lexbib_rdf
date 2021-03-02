160910275cdc734aa4a770f38e71b83c52971d2d
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-03-02 15:25:22 +0100

    Update wditest.py

--- wikibase/wditest.py
+++ wikibase/wditest.py
@@ -1,6 +1,8 @@
 
 from wikidataintegrator import wdi_core, wdi_login
 
+# This is an attempt to write a statement with datatype Time to data.lexbib.org wikibase instance
+
 lwbuser = "DavidL"
 with open('D:/LexBib/wikibase/data_lexbib_org_pwd.txt', 'r', encoding='utf-8') as pwdfile:
 	lwbpass = pwdfile.read()
@@ -8,6 +10,8 @@ mediawiki_api_url = "https://data.lexbib.org/w/api.php" # <- change to applicabl
 sparql_endpoint_url = "https://data.lexbib.org/query/sparql"  # <- change to applicable wikibase
 login = wdi_login.WDLogin(lwbuser, lwbpass, mediawiki_api_url=mediawiki_api_url)
 print(login)
+
+# This does not have any effect:
 # wdi_core.config['PROPERTY_CONSTRAINT_PID'] = "P7"
 # wdi_core.config['DISTINCT_VALUES_CONSTRAINT_QID'] = "Q16512"
 # print(str(wdi_core.config))
