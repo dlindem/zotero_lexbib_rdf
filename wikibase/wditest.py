3c45702008db874abee7713cc712f9ee60f2b4e8
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-03-02 23:51:48 +0100

    Update wditest.py

--- wikibase/wditest.py
+++ wikibase/wditest.py
@@ -1,12 +1,14 @@
 # This is an attempt to write a statement with datatype Time to data.lexbib.org wikibase instance
-from wikidataintegrator import wdi_core, wdi_login
-from wikidataintegrator.wdi_config import config as wdi_config
 
-wdi_config['MEDIAWIKI_API_URL'] = 'https://data.lexbib.org/w/api.php'
-wdi_config['SPARQL_ENDPOINT_URL'] = 'https://data.lexbib.org/query/sparql'
-wdi_config['WIKIBASE_URL'] = 'http://data.lexbib.org'
-wdi_config['PROPERTY_CONSTRAINT_PID'] = 'P7'
-wdi_config['DISTINCT_VALUES_CONSTRAINT_QID'] = 'Q16512'
+from wikidataintegrator import wdi_core, wdi_login
+
+# from wikidataintegrator.wdi_config import config as wdi_config
+#
+# wdi_config['MEDIAWIKI_API_URL'] = 'https://data.lexbib.org/w/api.php'
+# wdi_config['SPARQL_ENDPOINT_URL'] = 'https://data.lexbib.org/query/sparql'
+# wdi_config['WIKIBASE_URL'] = 'http://data.lexbib.org'
+# wdi_config['PROPERTY_CONSTRAINT_PID'] = 'P7'
+# wdi_config['DISTINCT_VALUES_CONSTRAINT_QID'] = 'Q16512'
 
 
 lwbuser = "DavidL"
@@ -15,28 +17,20 @@ with open('D:/LexBib/wikibase/data_lexbib_org_pwd.txt', 'r', encoding='utf-8') a
 mediawiki_api_url = "https://data.lexbib.org/w/api.php" # <- change to applicable wikibase
 sparql_endpoint_url = "https://data.lexbib.org/query/sparql"  # <- change to applicable wikibase
 login = wdi_login.WDLogin(lwbuser, lwbpass, mediawiki_api_url=mediawiki_api_url)
-print(login)
-
-# This does not have any effect:
-# wdi_core.config['PROPERTY_CONSTRAINT_PID'] = "P7"
-# wdi_core.config['DISTINCT_VALUES_CONSTRAINT_QID'] = "Q16512"
-# print(str(wdi_core.config))
-
-wikibaseEntityEngine = wdi_core.WDItemEngine.wikibase_item_engine_factory(mediawiki_api_url, sparql_endpoint_url)
-print(wikibaseEntityEngine)
+lwbEngine = wdi_core.WDItemEngine.wikibase_item_engine_factory(mediawiki_api_url, sparql_endpoint_url)
 
 lwbqid = "Q307"
 lwbprop = "P15"
-value = '+2001-12-31T12:01:13Z'
+value = '+2013-01-01T00:00:00Z'
 
 data = []
 data.append(wdi_core.WDTime(value, prop_nr=lwbprop))
-data.append(wdi_core.WDItemID(value=lwbqid, prop_nr=lwbprop))
-print(str(data))
+#data.append(wdi_core.WDItemID(value=lwbqid, prop_nr=lwbprop))
+#print(str(data))
 
-item = wikibaseEntityEngine(data=data)
+item = lwbEngine(wd_item_id=lwbqid, data=data)
 
-print(item.write(login))
+print('Success: '+item.write(login))
 
 # If you change this line:
 #
